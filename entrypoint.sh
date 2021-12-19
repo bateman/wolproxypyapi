90a480c63f5ab844f55146bd730f622e8b9eb6de
Fabio Calefato <fabio.calefato@uniba.it>
2021-12-19 19:16:29 +0100

    Fix small misconfigurations with logs handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # this script is used to boot the Docker container
 
-mkdir -p /logs/gunicorn
 touch /logs/error.log
 touch /logs/info.log
-chmod -R +r /logs/gunicorn
 
 exec gunicorn wolproxypyapi.main:app \
     --bind=0.0.0.0:80 \
