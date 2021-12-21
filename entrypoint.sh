ab41195760b3040d8727a7fbe00449c338df25c1
Fabio Calefato <fcalefato@gmail.com>
2021-12-21 14:10:35 +0100

    Rename log files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 # this script is used to boot the Docker container
 
-mkdir -p logs
-touch /logs/error.log
-touch /logs/info.log
-chmod -R 777 logs
+touch /logs/server.log
+touch /logs/app.log
 
 exec gunicorn wolproxypyapi.main:app \
     --bind=0.0.0.0:8000 \
     --workers=2 \
     --worker-class=uvicorn.workers.UvicornWorker \
     --log-level=info \
-    --error-logfile=/logs/error.log \
-    --access-logfile=/logs/info.log
+    --log-file=/logs/server.log \
+    --access-logfile=/logs/app.log
