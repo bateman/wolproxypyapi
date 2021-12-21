c84f36636fb64a54c8e6b16283c6304bd78768df
Fabio Calefato <fabio.calefato@uniba.it>
2021-12-21 08:47:27 +0100

    Fix wrong bind port

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ touch /logs/info.log
 chmod -R 777 logs
 
 exec gunicorn wolproxypyapi.main:app \
-    --bind=0.0.0.0:80 \
+    --bind=0.0.0.0:8000 \
     --workers=2 \
     --worker-class=uvicorn.workers.UvicornWorker \
     --log-level=info \
