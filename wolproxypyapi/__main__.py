ca3cf63dde40fdd0458f7ab2913397343777bd31
Fabio Calefato <fabio.calefato@uniba.it>
2024-02-09 17:06:23 +0100

    Add main entry point to wolproxypyapi module

---
+++ wolproxypyapi/__main__.py
@@ -0,0 +1,9 @@
+"""The main module of the wolproxypyapi application.
+
+This is responsible for running the application.
+"""
+
+from wolproxypyapi import main
+
+if __name__ == "__main__":
+    main.run()
